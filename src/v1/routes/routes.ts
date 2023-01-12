621aec8ee64b26fb5400f0fc7fd6b18a67b2ea1e
Pedro <pedrovs3@hotmail.com>
2023-01-12 15:42:38 -0300

    netlify connection

--- src/v1/routes/routes.ts
+++ src/v1/routes/routes.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from 'fastify';
 
 // Declaration of an api route
 export async function testRoutes(fastify: FastifyInstance) {
-  fastify.get('/test/', async (request, reply) => {
+  fastify.get('/', async (request, reply) => {
     reply.send({ message: 'Hello!' });
   });
 }
