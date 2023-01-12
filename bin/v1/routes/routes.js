24ef0943dca6e960eb52f0daac897de1a56ae9b8
Pedro <pedrovs3@hotmail.com>
2023-01-12 15:44:36 -0300

    netlify connection

--- bin/v1/routes/routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.testRoutes = void 0;
-async function testRoutes(fastify) {
-    fastify.get('/test/', async (request, reply) => {
-        reply.send({ message: 'Hello!' });
-    });
-}
-exports.testRoutes = testRoutes;
