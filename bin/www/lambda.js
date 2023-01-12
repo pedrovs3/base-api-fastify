15fa8700c39b8921748cbc73781cf480e2649a99
Pedro <pedrovs3@hotmail.com>
2023-01-12 15:29:37 -0300

    netlify connection

--- bin/www/lambda.js
+++ bin/www/lambda.js
@@ -1,4 +1,7 @@
 import awsLambdaFastify from '@fastify/aws-lambda';
 import server from './server';
 
-export const proxy = awsLambdaFastify(server);
+const proxy = awsLambdaFastify(server);
+const handler = proxy;
+
+export { handler };
